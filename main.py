f030ddedfdb8b0ce1e8ce7f8acd931de627f0a72
Holman Alvarez <holman.alvarezm@gmail.com>
2023-05-12 18:26:49 -0500

    Modify APIs for use local database

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from starlette.requests import Request
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
-from models.movie import Movie
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = 'My App of Movies'
@@ -34,13 +35,13 @@ class Movie(BaseModel):
     overview: str = Field(min_length=15, max_length=50)
     year: int = Field(le=2023)
     rating: int = Field(gt=0, le=10)
-    category: str = Field(min_length=5, max_length=10)
+    category: str = Field(min_length=5, max_length=15)
     
     class Config:
         schema_extra = {
             "example": {
                 "id": 1,
-                "title": "No named",
+                "title": "No named movie",
                 "overview": "Overview of movie",
                 "year": 1900,
                 "rating": 1.0,
@@ -85,7 +86,9 @@ def get_movies() -> List[Movie]:
         Parameters: None
         Returns: Json with all the movies in database
     """
-    return JSONResponse(status_code=200, content=movies)
+    db = Session()
+    result = db.query(MovieModel).all()    
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.get('/movies/{id}', tags=['Movies'], response_model=Movie, status_code=200)
 def get_movie(id: int = Path(ge=1, le=10000)) -> Movie:
@@ -94,8 +97,12 @@ def get_movie(id: int = Path(ge=1, le=10000)) -> Movie:
         Parameters: id (int): identificator of movie
         Returns: Json with data of the selected movie or empty list if don't exist id
     """
-    data = [movie for movie in movies if movie['id'] == id]
-    return JSONResponse(status_code=200, content=data)
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "Not found"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.get('/movies/', tags=['Movies'], response_model=List[Movie], status_code=200)
 def get_movies_by_category(category: str = Query(min_length=5, max_length=20)) -> List[Movie]:
@@ -104,44 +111,45 @@ def get_movies_by_category(category: str = Query(min_length=5, max_length=20)) -
         Parameters: category (str): category of search
         Returns: Json with movies of category or empty list if don't exist id
     """
-    data = [movie for movie in movies if movie['category'] == category]
-    return JSONResponse(status_code=200, content=data)
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    
+    if not result:
+        return JSONResponse(status_code=404, content={"message":"Not result data"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
 @app.post('/movies', tags=['Movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
+    db = Session()
+    new_movie = MovieModel(**movie.dict())
+    db.add(new_movie)
+    db.commit()
     return JSONResponse(status_code=201, content={"message":"Movie was register"})
-
-""" @app.post('/movies', tags=['Movies'])
-async def create_movie(request: Request):
-    movie = await request.json()
-    movies.append(movie)
-    return movies
-"""
-
-""" @app.put('/movies/{id}', tags=['Movies'])
-async def update_movie(id: int, request: Request):
-    movie = await request.json()
-    for index, item in enumerate(movies):
-        if item["id"] == id:
-            movies[index].update(movie)
-            return movies[index]
-        
-    raise HTTPException(status_code=404, detail='Movie not found') 
-"""
 
 @app.put('/movies/{id}', tags=['Movies'], response_model=dict, status_code=200)
 def update_movie(id: int, movie: Movie) -> dict:
-    for index, item in enumerate(movies):
-        if item["id"] == id:
-            movies[index].update(movie)
-            movies[index]["id"] = id
-            return JSONResponse(status_code=200, content={"message":"Movie was modify"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    
+    if not result:
+        return JSONResponse(status_code=404, content={"message": "Not found"})
+    
+    result.title = movie.title
+    result.overview = movie.overview
+    result.year = movie.year
+    result.rating = movie.rating
+    result.category = movie.category
+    db.commit()
+    return JSONResponse(status_code=200, content={"message":"Movie was modify"})
 
 @app.delete('/movies/{id}', tags=['Movies'], response_model=dict, status_code=200)
 async def delete_movie(id: int) -> dict:
-    for index, item in enumerate(movies):
-        if item["id"] == id:
-            del movies[index]
-            return JSONResponse(status_code=200, content={"message":"Movie was delete"})
-    raise HTTPException(status_code=404, detail="Movie not found")
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    
+    if not result:
+        raise HTTPException(status_code=404, detail="Movie not found")
+               
+    db.delete(result)
+    db.commit()
+    raise HTTPException(status_code=200, detail="Movie was delete")    
